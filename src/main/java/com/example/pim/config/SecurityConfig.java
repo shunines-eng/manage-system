8acd24b67299fe4da96dc6fee4728ee4112be830
shunines <3298978149@qq.com>
2025-09-07 18:48:09 +0800

    'feat(security): 密码采用md5加密方式保存'

--- src/main/java/com/example/pim/config/SecurityConfig.java
+++ src/main/java/com/example/pim/config/SecurityConfig.java
@@ -30,7 +30,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        // 使用MD5加密方式保存密码
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence rawPassword) {
+                try {
+                    java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+                    byte[] array = md.digest(rawPassword.toString().getBytes());
+                    StringBuffer sb = new StringBuffer();
+                    for (int i = 0; i < array.length; ++i) {
+                        sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+                    }
+                    return sb.toString();
+                } catch (java.security.NoSuchAlgorithmException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public boolean matches(CharSequence rawPassword, String encodedPassword) {
+                return encode(rawPassword).equals(encodedPassword);
+            }
+        };
     }
 
     @Override
